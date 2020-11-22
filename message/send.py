fe7c39288b4fe8ccaf42791870aab5d0fef55df5
krishukr <2655290588@qq.com>
2020-11-22 11:36:21 +0800

    message包 发私信 文字私信 修改注释

--- message/send.py
+++ message/send.py
@@ -3,7 +3,16 @@ import json
 import requests
 
 
-def send_message_text(SESSDATA, token, sender, receiver, content):
+def send_message_text(SESSDATA: str, token: str, sender: int, receiver: int, content: str):
+    """
+    发送文字私信
+    :param SESSDATA: 登录token，可在cookies获取
+    :param token: 私信验证token，可在cookies-bili_jct获取
+    :param sender: 发送者uid
+    :param receiver: 收信者uid
+    :param content: 文字内容
+    :return: 为空
+    """
     try:
         data = requests.post(url="http://api.vc.bilibili.com/web_im/v1/web_im/send_msg",
                              cookies={"SESSDATA": SESSDATA},
@@ -26,5 +35,5 @@ if __name__ == '__main__':
     send_message_text("6ba530d5%2C1620206403%2Cf1ba2*b1",
                       "a5b50cf4773f13debd8e90a4f7961ce2",
                       325272949,
-                      37090048,
-                      "Hello World!\nby Python")
+                      437952226,
+                      "Hello World!\nby Python[doge]")
