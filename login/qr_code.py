1960365aef86fda8556dcd7130e96949275989f8
krishukr <2655290588@qq.com>
2020-11-22 10:40:27 +0800

    login包 二维码登录 获取登陆二维码

--- login/qr_code.py
+++ login/qr_code.py
@@ -1,1 +1,19 @@
 # coding=utf-8
+import requests
+import json
+
+
+def get_qr_code():
+    """获取登录二维码和密钥
+    :return:二维码地址，密钥
+    """
+    try:
+        data = requests.get(url="http://passport.bilibili.com/qrcode/getLoginUrl")
+        data = json.loads(data.text)["data"]
+        return data["url"], data["oauthKey"]
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    print(get_qr_code())
