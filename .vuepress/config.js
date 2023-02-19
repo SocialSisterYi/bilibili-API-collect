2d6229928ee6582eaf1c0ac1a3ee11a3a192faa7
Sugarscat <tsugarcat@gmail.com>
2023-02-19 17:07:37 +0800

    尝试修复图标显示异常

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,10 +6,12 @@ module.exports = {
         },
     },
     head: [
-        ['link', { rel: 'icon', href: '/logo.svg' }]
+        ['link', { rel: 'icon', href: './logo.png' }]
 
     ],
     title: "哔哩哔哩-API",
+    description: "本项目旨在对 B站 WEB、APP、TV 等客户端中，散落在世界各地的野生 API 进行收集整理，研究使用方法并对其进行说明，" +
+        "运用了黑箱法、控制变量法、代码逆向分析、拆包及反编译法、网络抓包法等研究办法。",
     themeConfig: {
         nav: [
             { text: '介绍', link: '/' },
