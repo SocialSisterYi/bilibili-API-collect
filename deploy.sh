da60ccedc46de439c2e0c25c43563e9ea6e40d59
Sugarscat <tsugarcat@gmail.com>
2023-02-19 16:42:16 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Sugarscat/bilibili-API-collect.git master:gh-pages
+git push -f git@github.com:sugarscat/bilibili-API-collect.git master:gh-pages
 
 cd -
