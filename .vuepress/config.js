6824f74b0c259c6758bf67852c5a927c0111b03e
SocialSisterYi <1440239038@qq.com>
2023-02-21 09:03:46 +0800

    🚀add pages auto deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,9 +9,8 @@ module.exports = {
         ['link', { rel: 'icon', href: './logo.png' }]
 
     ],
-    title: "哔哩哔哩-API",
-    description: "本项目旨在对 B站 WEB、APP、TV 等客户端中，散落在世界各地的野生 API 进行收集整理，研究使用方法并对其进行说明，" +
-        "运用了黑箱法、控制变量法、代码逆向分析、拆包及反编译法、网络抓包法等研究办法。",
+    title: "bilibili-API-collect",
+    description: "本项目旨在对 B站 WEB、APP、TV 等客户端中，散落在世界各地的野生 API 进行收集整理，研究使用方法并对其进行说明。",
     themeConfig: {
         nav: [
             { text: '介绍', link: '/' },
