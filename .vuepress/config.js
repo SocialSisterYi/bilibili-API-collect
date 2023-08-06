c48b1d203d9999d7054cfde294c1d044ecf3142b
OceanJiang <catlair@qq.com>
2023-08-06 19:00:43 +0800

    vuepress add Edit this page

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -22,11 +22,13 @@ module.exports = {
                     {text: 'QQ交流群', link: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=2rRJXK3zgU5yi3e_4rNLeJYUjMLzw_Jj&authKey=L%2FN3EfZXo3QFjEuHq4ifTgh%2F384UmrYpjl7dbYPhYHtznhfJWxkymxQKObQunmEQ&noverify=0&group_code=560304737'},
                     {text: 'Telegram交流群', link: 'https://t.me/bilibili_API_collect_community'}
                 ]
-            },
-            {text: 'GitHub', link: 'https://github.com/SocialSisterYi/bilibili-API-collect'},
+            }
         ],
         sidebar: 'auto',
-        smoothScroll: true
+        smoothScroll: true,
+        repo: 'SocialSisterYi/bilibili-API-collect',
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页',
     },
     plugins: [
         '@vuepress/back-to-top',
